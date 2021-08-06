595f1d3af4fd1b31da9284222bddcac666a7db11
gilfanovrrPaker <gilfanovrr@npf-paker.ru>
2021-08-06 11:59:41 +0500

    threads

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,45 @@
+use std::sync::mpsc;
 fn main() {
-    println!("Number of logical cores is {}", num_cpus::get());
+    // println!("Number of logical cores is {}", num_cpus::get());
     let mut v = Vec::new();
     v.push(1);
-    let f = |x: i32| x;
-    split_work(v, f2);
+    v.push(2);
+    v.push(3);
+    let f = |x: i32| x * 2;
+    let r = split_work(v, f);
+    for i in r {
+        println!("{}", i)
+    }
 }
 
-fn split_work<T>(v: Vec<T>, &f: dyn Fn(T) -> T) -> Vec<T> {
-    let num_cpu = num_cpus::get();
+fn split_work<T: Copy + std::marker::Send + 'static, F>(v: Vec<T>, f: F) -> Vec<T>
+where
+    F: Fn(T) -> T + Send + Copy + 'static + std::marker::Sync,
+{
+    let num_cpu = 2;
+    let mut r = Vec::new();
+    let (tx, rx) = mpsc::channel();
+    let chunk_size = v.len() / num_cpu;
+    let chunks = v.chunks(chunk_size);
+    for chunk in chunks {
+        let ch = chunk.to_vec();
+        let tx1 = tx.clone();
+        std::thread::spawn(move || {
+            for i in ch.clone().into_iter() {
+                let j = f(i.clone());
+                tx1.send(j).unwrap();
+            }
+        });
+    }
 
-    f(v[0]);
-    // for i in 0..5 {
-    //     std::thread::spawn(move || {
-    //         let args = Docopt::new(USAGE)
-    //             .and_then(|dopt| dopt.parse())
-    //             .unwrap_or_else(|e| e.exit());
-    //         let config = mssql_config(args.to_owned());
-    //         // let tcp: TcpStream;
-    //         // let mut client: Client<TcpStream>;
-    //         // println!("  instance: {}", instance);
-    //         // if instance == "" {
-    //         // config.port(1433);
-    //     });
-    // }
-    v
-}
-
-fn f2(x: i32) {
-    x
+    let mut i = v.len();
+    loop {
+        let received = rx.recv().unwrap();
+        r.push(received);
+        i -= 1;
+        if i == 0 {
+            break;
+        };
+    }
+    r
 }
